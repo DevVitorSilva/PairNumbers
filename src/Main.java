9cf285fd2cf388671928d4618648a35968564304
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-28 19:01:26 -0300

    declaring variables, using for loops and printing the results

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("How many numbers will you enter? ");
         int quantity = sc.nextInt();
+        int[] numbers = new int[quantity];
+        int quantityNumbersPair = 0;
+        for(int i = 0; i < quantity; i++){
+            System.out.print("Enter a number: ");
+            numbers[i] = sc.nextInt();
+        }
+        System.out.println("Pair numbers:");
+        for(int i = 0; i < quantity; i++){
+            if(numbers[i] % 2 == 0){
+                System.out.print(numbers[i] + " ");
+                quantityNumbersPair++;
+            }
+        }
+        System.out.println("\nNumber pair quantity: " + quantityNumbersPair);
         sc.close();
     }
 }
